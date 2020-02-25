c91969ea1c741e9bc32aada5d425a2c248259ce2
Jakiepoo <jakiepoo@MacBook-Pro-3.local>
2020-02-25 10:30:23 -0800

    Initial commit

--- twitterclone/twitteruser/models.py
+++ twitterclone/twitteruser/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class TwitterUser(AbstractUser):
+    #don't know what properties I want yet
+    favorite_color = models.CharField(max_length=20)
+    a = models.IntegerField(null=True, blank=True)
